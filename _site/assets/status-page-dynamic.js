da562e074c0cc3c29ad58d86451b692d2f6b700f
Robert Rawlins <robert@supporttime.com>
2013-06-25 11:12:28 +0100

    Removed pusher integration for the moemnt as it's not required.

--- _site/assets/status-page-dynamic.js
+++ _site/assets/status-page-dynamic.js
@@ -1,39 +1,10 @@
 // Initilize the Angular App.
 var myApp = angular.module('myApp', []);
 
-// Create a filter for finding items by ID.
-myApp.filter('find_by_id', function() {
-  return function(input, id) {
-    var i=0, len=input.length;
-    for (; i<len; i++) {
-      if (+input[i].id == +id) {
-        return input[i];
-      }
-    }
-    return null;
-  }
-});
-
 // Defined a controller for the page.
 var PageCtrl = ['$scope', '$filter', '$http', function($scope, $filter, $http) {
-	// Define the connection to the pusher service.
-	$scope.pusher = new Pusher($('body').data('key'));
-	$scope.channel = $scope.pusher.subscribe($('body').data('channel'));
-
 	// Laod in the apologies to start with.
 	$scope.page = JSON.parse($('#apologies-data').text());
-
-	// This method tells us whether the page is in appology state or not.
-	$scope.open_apologies = function() {
-		// Get a filered array of open apologies.
-		return $filter('filter')($scope.page.apologies, {state:'open'});
-	};
-
-	// This method tells us whether the page is in appology state or not.
-	$scope.closed_apologies = function() {
-		// Get a filered array of open apologies.
-		return $filter('filter')($scope.page.apologies, {state:'closed'});
-	};		
 
 	// This method tells us whether the page is in appology state or not.
 	$scope.sorry = function() {
@@ -49,25 +20,4 @@ var PageCtrl = ['$scope', '$filter', '$http', function($scope, $filter, $http) {
 			return true
 		};
 	};
-
-	/* Bind for the apology updated event. */
-	$scope.channel.bind('apology-updated', function(data) {
-		// An apology has been updated, we must update the model.
-		$scope.$apply(function(){
-			// Update the appropriate record with new details.
-			var found = $filter('find_by_id')($scope.page.apologies, data.id);
-
-			// Extend the element that we found.
-			$.extend(true, found, data);
-		});
-	});
-
-	// Bind for the apology created event.
-    $scope.channel.bind('apology-created', function(data) {
-    	// A new apology has been created.
-      	$scope.$apply(function(){
-      		// Append it to the scope collection.
-	      	$scope.page.apologies.push(data);
-	    });
-    });
 }];
