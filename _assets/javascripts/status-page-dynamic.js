84e632d58668644090f3041038fc32f98b6bd875
Robert Rawlins <robert@supporttime.com>
2013-06-25 17:21:25 +0100

    Tidy up of the way that filters are applied to the page scope, also confitional show of carousal nav so only when more than single item.

--- _assets/javascripts/status-page-dynamic.js
+++ _assets/javascripts/status-page-dynamic.js
@@ -7,12 +7,21 @@ var PageCtrl = ['$scope', '$filter', '$http', function($scope, $filter, $http) {
 	$scope.page = JSON.parse($('#apologies-data').text());
 
 	// This method tells us whether the page is in appology state or not.
-	$scope.sorry = function() {
+	$scope.current_apologies = function() {
 		// Get a filered array of open apologies.
-		var open_apologies = $filter('filter')($scope.page.apologies, {state:'open'});
+		return $filter('filter')($scope.page.apologies, {state:'open'});
+	};
 
+	// This method tells us whether the page is in appology state or not.
+	$scope.previous_apologies = function() {
+		// Get a filered array of open apologies.
+		return $filter('filter')($scope.page.apologies, {state:'closed'});
+	};
+
+	// This method tells us whether the page is in appology state or not.
+	$scope.sorry = function() {
 		// Cbeck the number of open apologies.
-		if(open_apologies.length == 0) {
+		if($scope.current_apologies().length == 0) {
 			// We have none, so return success state.
 			return false
 		} else {
