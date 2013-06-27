954d25ca2eb15538b0ee67d60ae9993d9d6d8ac8
Robert Rawlins <robert@supporttime.com>
2013-06-27 16:24:31 +0100

    Switched angular inline templates for additional template files which give us better control over slides and carousel formatting.

--- _site/assets/angular-carousel.js
+++ _site/assets/angular-carousel.js
@@ -1,5 +1,4 @@
-angular.module("ui.bootstrap", ["ui.bootstrap.tpls", "ui.bootstrap.transition","ui.bootstrap.carousel"]);
-angular.module("ui.bootstrap.tpls", ["template/carousel/carousel.html","template/carousel/slide.html"]);
+angular.module("ui.bootstrap", ["ui.bootstrap.transition","ui.bootstrap.carousel"]);
 angular.module('ui.bootstrap.transition', [])
 
 /**
@@ -413,29 +412,4 @@ function CarouselDemoCtrl($scope) {
       });
     }
   };
-}]);
-
-angular.module("template/carousel/carousel.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("template/carousel/carousel.html",
-    "<div class=\"carousel\">\n" +
-    "    <ol class=\"carousel-indicators\" ng-show=\"slides().length > 1\">\n" +
-    "        <li ng-repeat=\"slide in slides()\" ng-class=\"{active: isActive(slide)}\" ng-click=\"select(slide)\"></li>\n" +
-    "    </ol>\n" +
-    "    <div class=\"carousel-inner\" ng-transclude></div>\n" +
-    "    <a ng-click=\"prev()\" class=\"carousel-control left\" ng-show=\"slides().length > 1\">&lsaquo;</a>\n" +
-    "    <a ng-click=\"next()\" class=\"carousel-control right\" ng-show=\"slides().length > 1\">&rsaquo;</a>\n" +
-    "</div>\n" +
-    "");
-}]);
-
-angular.module("template/carousel/slide.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("template/carousel/slide.html",
-    "<div ng-class=\"{\n" +
-    "    'active': leaving || (active && !entering),\n" +
-    "    'prev': (next || active) && direction=='prev',\n" +
-    "    'next': (next || active) && direction=='next',\n" +
-    "    'right': direction=='prev',\n" +
-    "    'left': direction=='next'\n" +
-    "  }\" class=\"item\" ng-transclude></div>\n" +
-    "");
 }]);
